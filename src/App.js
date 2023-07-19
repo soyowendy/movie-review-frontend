86fb852dad19bbcbb7d8b5abad9076440e56f8e0
Wenderson Santos <souowendy@gmail.com>
2023-07-19 08:57:16 -0300

    Create Home and Hero Component and show Movie Catalog in Carousel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import api from './api/axiosConfig';
 import {useState, useEffect} from 'react';
+import Layout from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
   const [movies, setMovies] = useState();
@@ -8,7 +11,6 @@ function App() {
   const getMovies = async () => {
     try {
       const response = await api.get("/api/v1/movies");
-      console.log(response.data);
       setMovies(response.data);
     } catch(err) {
       console.log(err);
@@ -21,6 +23,11 @@ function App() {
 
   return (
     <div className="App">
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route path='/' element={<Home movies={movies}/>}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/home/Home.js
@@ -0,0 +1,9 @@
+import Hero from "../Hero/Hero";
+
+const Home = ({movies}) => {
+    return (
+        <Hero movies={movies}/>
+    )
+}
+
+export default Home;
